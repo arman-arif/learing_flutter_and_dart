044a559b4d01b6e9aef7b30667ad218df4741e05
Arman Arif <arman3472rmn@gmail.com>
2021-09-25 15:51:42 +0600

    Personal information printing

---
+++ basics_dart_codes/program1.dart
@@ -0,0 +1,16 @@
+void main() {
+  int num1 = 34;
+  double num2 = 45.787;
+
+  int total = num1 + num2.toInt();
+
+  print('Total: $total');
+
+  String name = 'Arman Arif';
+  String address = 'Digholia, Khulna';
+  String position = 'Assistant Engineer';
+  double salary = 29600.00;
+  double alounce = 2368.00;
+  print(
+      "Details: \n Name: $name, \n Address: $address, \n Possition: $position");
+}
