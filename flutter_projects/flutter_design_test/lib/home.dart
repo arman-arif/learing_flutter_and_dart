9c932f0b6eaab884931c201103fb6f8928ee80f6
Arman Arif <arman3472rmn@gmail.com>
2021-09-27 10:43:45 +0600

    Moved to parent directory

---
+++ flutter_projects/flutter_design_test/lib/home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: 400,
+          width: 300,
+          decoration: BoxDecoration(
+              color: Colors.amberAccent,
+              borderRadius: BorderRadius.circular(7)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 200,
+                color: Colors.lightBlue,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "Ariful Islam",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 17),
+                    ),
+                    Text("Address: Khulna"),
+                    Text("Skills: PHP, JS, Flutter"),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(50)),
+                height: 50,
+                width: 295,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Dhaka"),
+                    Text("Khulna"),
+                    Text("Chittagong"),
+                    Text("Barisal"),
+                    Text("Rajshahi"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
