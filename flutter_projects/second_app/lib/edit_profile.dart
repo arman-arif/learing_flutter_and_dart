2c555d3410cd32f703e99cff801039bf426e7253
Arman Arif <arman3472rmn@gmail.com>
2021-10-07 23:28:00 +0600

    Added edit profile page design

--- flutter_projects/second_app/lib/edit_profile.dart
+++ flutter_projects/second_app/lib/edit_profile.dart
@@ -14,6 +14,7 @@ class _EditPageState extends State<EditPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromRGBO(35, 151, 142, 1),
         title: Text(
           "Edit Profile",
           style: TextStyle(
@@ -33,18 +34,71 @@ class _EditPageState extends State<EditPage> {
               ),
             );
           },
-          icon: Icon(Icons.arrow_back_ios),
-          iconSize: 20,
+          icon: Icon(Icons.person_outline),
+          iconSize: 30,
           color: Colors.white70,
         ),
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.check),
-            iconSize: 20,
-            color: Colors.white70,
+          Container(
+            margin: EdgeInsets.only(
+              right: 10,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 7,
+              ),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProfilePage(),
+                    ),
+                  );
+                },
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(7),
+                  side: BorderSide(
+                    color: Color.fromRGBO(255, 255, 255, 0.5),
+                    width: 3,
+                  ),
+                ),
+                color: Color.fromRGBO(255, 255, 255, 0.2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "Save",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Icon(
+                      Icons.check,
+                      size: 20,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
+      ),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width - 50,
+          child: Text(
+            "This is Edit Profile Page",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.w600,
+              color: Color.fromRGBO(245, 90, 40, 1),
+            ),
+          ),
+        ),
       ),
     );
   }
